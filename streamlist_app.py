39d5d3b910066c663e242f06ab326b76add55dfe
swaroopsnow <126833363+swaroopsnow@users.noreply.github.com>
2023-03-03 11:21:03 -0600

    Update streamlist_app.py

--- streamlist_app.py
+++ streamlist_app.py
@@ -1,4 +1,6 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-streamlit.text('abc')
+streamlit.text('Omega-3 & Blueberry oatmeal \n')
+streamlit.text('Kale, Spinach & Rocket Smoothie \n')
+streamlit.text('Hard boiled free range eggs')
