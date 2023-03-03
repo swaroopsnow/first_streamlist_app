708524ba3187f1dc5c42acfcfc5cbd733933fb27
swaroopsnow <126833363+swaroopsnow@users.noreply.github.com>
2023-03-03 11:36:57 -0600

    Update streamlist_app.py

--- streamlist_app.py
+++ streamlist_app.py
@@ -2,6 +2,9 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu \n')
-streamlit.text('Omega-3 & Blueberry oatmeal \n')
-streamlit.text('Kale, Spinach & Rocket Smoothie \n')
-streamlit.text('Hard boiled free range eggs')
+streamlit.text('🥣Omega-3 & Blueberry oatmeal \n')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie \n')
+streamlit.text('🐔Hard boiled free range eggs')
+streamlit.text('🥑🍞 Avacado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+   
